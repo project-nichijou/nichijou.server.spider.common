b2a0efbfcd5fc7e3590563eb36b080e51205be49
JeffersonQin <1247006353@qq.com>
2021-07-11 23:02:01 +0800

    [Add] headers to settings

--- config/settings_template.py
+++ config/settings_template.py
@@ -12,3 +12,19 @@ DATABASE_CONFIG = {
 	'port': 3306,
 	'database': 'bangumi'
 }
+
+# request headers
+HEADERS = {
+	'Connection': 'keep-alive',
+	'Cache-Control': 'max-age=0',
+	'sec-ch-ua': '" Not;A Brand";v="99", "Microsoft Edge";v="91", "Chromium";v="91"',
+	'sec-ch-ua-mobile': '?0',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 Edg/91.0.864.59',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+	'Sec-Fetch-Site': 'none',
+	'Sec-Fetch-Mode': 'navigate',
+	'Sec-Fetch-User': '?1',
+	'Sec-Fetch-Dest': 'document',
+	'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6'
+}
