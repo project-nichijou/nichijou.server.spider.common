4640fdd0abb1177656187ea8a3c9eee3b0d9fefc
JeffersonQin <1247006353@qq.com>
2021-07-11 11:12:44 +0800

    [Delete] debug clauses

--- utils/hash.py
+++ utils/hash.py
@@ -2,9 +2,6 @@ import hashlib
 
 
 def get_md5(content: str):
-	print('5')
 	md5_obj = hashlib.md5()
-	print('7')
 	md5_obj.update(content.encode())
-	print('9')
 	return str(md5_obj.hexdigest()).upper()
