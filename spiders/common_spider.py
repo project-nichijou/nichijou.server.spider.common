1dce41d97193335d2be1ab75326992b75dd08c9f
JeffersonQin <1247006353@qq.com>
2021-07-13 01:24:02 +0800

    [Add feature] parameter: fail

--- spiders/common_spider.py
+++ spiders/common_spider.py
@@ -14,6 +14,33 @@ class CommonSpider(scrapy.Spider):
 	use_cookies = False
 
 
+	def __init__(self, fail='false', *args, **kwargs):
+		# use super class's method to initialize
+		super(CommonSpider, self).__init__(*args, **kwargs)
+		# call customized init method
+		self.initialize()
+		# define data source
+		if fail == 'on' or fail == 'true' or fail == '1' or fail == True or fail == 1:
+			self.init_fail_datasource()
+		else:
+			self.init_normal_datasource()
+
+
+	# this should be implemented
+	def initialize(self):
+		pass
+
+
+	# this should be implemented
+	def init_normal_datasource(self):
+		pass
+
+
+	# this should be implemented
+	def init_fail_datasource(self):
+		pass
+
+
 	def request(self, url, callback, errback, cb_kwargs=None):
 		'''
 		warpper for scrapy.Request
@@ -34,15 +61,17 @@ class CommonSpider(scrapy.Spider):
 		request = failure.request
 		parameters = request.cb_kwargs
 		url = request.url
-		urlmd5 = get_md5(url)
+		url_md5 = get_md5(url)
 
 		yield CommonFailedRequestItem(
 			url = url,
-			urlmd5 = urlmd5,
+			url_md5 = url_md5,
 			spider = self.name,
 			desc = format_log(
 				info = 'exception caught in spider.',
 				values = {
-					'failure': failure
-				})
+					'failure': failure.__dict__,
+					'parameters': parameters
+				}
+			)
 		)
