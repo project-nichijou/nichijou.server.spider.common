a81ed5400566419c4c27aa1eeae7bc274aa4991c
JeffersonQin <1247006353@qq.com>
2021-07-17 12:58:15 +0800

    [Add] get_date

--- utils/datetime.py
+++ utils/datetime.py
@@ -7,6 +7,9 @@ def get_time_str_now():
 def get_time_str_from_timestamp(timestamp: int):
 	return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
 
+def get_date_str_now():
+	return time.strftime("%Y-%m-%d", time.localtime())
+
 def check_time_format(timestr: str):
 	try:
 		_ = time.strptime(timestr, "%Y-%m-%d %H:%M:%S")
